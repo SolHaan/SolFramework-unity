eeb9a78102da4d9a6478727daf071aa1117cdc39
SolHaan <hansol13084@gmail.com>
2022-05-26 18:54:46 +0900

    [Generic Singleton]

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
